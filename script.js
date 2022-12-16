08a6b2954237f8132d7e9f08f668a2dcc7943534
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-16 21:16:55 +0100

    Add a function to check if a whole row is all X or O

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const GameBoard = (() => {
     let gameContainer = document.querySelector(".gameboard"),
         squares = document.querySelectorAll(".square"),
         moveNum = 1,
-        spots = [
+        board = [
             ["", "", ""],
             ["", "", ""],
             ["", "", ""],
@@ -17,7 +17,26 @@ const GameBoard = (() => {
                 : (e.target.innerText = "O");
 
         moveNum++;
-        spots[squareIndex[0]][squareIndex[1]] = e.target.innerText;
+        board[squareIndex[0]][squareIndex[1]] = e.target.innerText;
+        checkWinning();
+    };
+
+    const checkWinning = () => {
+        if (moveNum >= 5) {
+            checkWinningHorizontally();
+        }
+    };
+
+    const checkWinningHorizontally = () => {
+        for (let row of board) {
+            if (
+                row.every((elem) => elem === "X") ||
+                row.every((elem) => elem === "O")
+            ) {
+                alert("Someone has won");
+                break;
+            }
+        }
     };
 
     squares.forEach((square) =>
@@ -26,6 +45,6 @@ const GameBoard = (() => {
 
     return {
         gameContainer,
-        spots,
+        board,
     };
 })();
