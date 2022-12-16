f983adde79119cea2884afaac3111401262713fd
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-16 09:58:19 +0100

    Add a function to fill spots array when clicking on squares

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ const GameBoard = (() => {
     let gameContainer = document.querySelector(".gameboard"),
         squares = document.querySelectorAll(".square"),
         moveNum = 1,
-        spots = [];
+        spots = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""],
+        ];
 
     const updateSquare = (e) => {
+        let squareIndex = JSON.parse(e.target.getAttribute("data-index"));
+
         moveNum % 2 !== 0
             ? (e.target.innerText = "X")
             : (e.target.innerText = "O");
 
         moveNum++;
+        spots[squareIndex[0]][squareIndex[1]] = e.target.innerText;
     };
 
-    squares.forEach((square) => square.addEventListener("click", updateSquare));
+    squares.forEach((square) =>
+        square.addEventListener("click", updateSquare, false)
+    );
 
     return {
         gameContainer,
         squares,
+        spots,
     };
 })();
