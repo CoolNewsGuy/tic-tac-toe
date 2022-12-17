e8ea37015076b0f6bdda47ac6c73b7c9897a335f
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-17 08:18:37 +0100

    Change indentation to 3 tabs

--- script.js
+++ script.js
@@ -1,98 +1,97 @@
 const Game = (() => {
-    let moveNum = 1;
+   let moveNum = 1;
 
-    const checkWinning = () => {
-        moveNum++;
-        if (moveNum >= 5) {
-            _checkWinningHorizontally();
-            _checkWinningVertically();
-            _checkWinningDiagonally();
-        }
-    };
+   const checkWinning = () => {
+      moveNum++;
+      if (moveNum >= 5) {
+         _checkWinningHorizontally();
+         _checkWinningVertically();
+         _checkWinningDiagonally();
+      }
+   };
 
-    const _checkWinningHorizontally = () => {
-        for (let row of GameBoard.board) {
-            if (
-                row.every((elem) => elem === "X") ||
-                row.every((elem) => elem === "O")
-            ) {
-                alert("Someone has won");
-                break;
-            }
-        }
-    };
+   const _checkWinningHorizontally = () => {
+      for (let row of GameBoard.board) {
+         if (
+            row.every((elem) => elem === "X") ||
+            row.every((elem) => elem === "O")
+         ) {
+            alert("Someone has won");
+            break;
+         }
+      }
+   };
 
-    const _checkWinningVertically = () => {
-        for (let elem of GameBoard.firstRow) {
-            if (
-                elem &&
-                elem ===
-                    GameBoard.secondRow[GameBoard.firstRow.indexOf(elem)] &&
-                elem === GameBoard.thirdRow[GameBoard.firstRow.indexOf(elem)]
-            ) {
-                alert("Yep true");
-                break;
-            }
-        }
-    };
+   const _checkWinningVertically = () => {
+      for (let elem of GameBoard.firstRow) {
+         if (
+            elem &&
+            elem === GameBoard.secondRow[GameBoard.firstRow.indexOf(elem)] &&
+            elem === GameBoard.thirdRow[GameBoard.firstRow.indexOf(elem)]
+         ) {
+            alert("Yep true");
+            break;
+         }
+      }
+   };
 
-    const _checkWinningDiagonally = () => {
-        if (
-            GameBoard.firstRow[0] &&
-            GameBoard.firstRow[0] === GameBoard.secondRow[1] &&
-            GameBoard.firstRow[0] === GameBoard.thirdRow[2]
-        ) {
-            alert("KO");
-        } else if (
-            GameBoard.firstRow[2] &&
-            GameBoard.firstRow[2] === GameBoard.secondRow[1] &&
-            GameBoard.firstRow[2] === GameBoard.thirdRow[0]
-        ) {
-            alert("Yeahhh");
-        }
-    };
+   const _checkWinningDiagonally = () => {
+      if (
+         GameBoard.firstRow[0] &&
+         GameBoard.firstRow[0] === GameBoard.secondRow[1] &&
+         GameBoard.firstRow[0] === GameBoard.thirdRow[2]
+      ) {
+         alert("KO");
+      } else if (
+         GameBoard.firstRow[2] &&
+         GameBoard.firstRow[2] === GameBoard.secondRow[1] &&
+         GameBoard.firstRow[2] === GameBoard.thirdRow[0]
+      ) {
+         alert("Yeahhh");
+      }
+   };
 
-    return {
-        checkWinning,
-        moveNum,
-    };
+   return {
+      checkWinning,
+      moveNum,
+   };
 })();
 
 const GameBoard = (() => {
-    let gameContainer = document.querySelector(".gameboard"),
-        _squares = document.querySelectorAll(".square"),
-        board = [
-            ["", "", ""],
-            ["", "", ""],
-            ["", "", ""],
-        ];
+   let gameContainer = document.querySelector(".gameboard"),
+      _squares = document.querySelectorAll(".square"),
+      board = [
+         ["", "", ""],
+         ["", "", ""],
+         ["", "", ""],
+      ];
 
-    const firstRow = board[0],
-        secondRow = board[1],
-        thirdRow = board[2];
+   const firstRow = board[0],
+      secondRow = board[1],
+      thirdRow = board[2];
 
-    const __updateSquare = (e) => {
-        let squareIndex = JSON.parse(e.target.getAttribute("data-index"));
+   const __updateSquare = (e) => {
+      let squareIndex = JSON.parse(e.target.getAttribute("data-index"));
 
-        if (!e.target.innerText)
-            Game.moveNum % 2 !== 0
-                ? (e.target.innerText = "X")
-                : (e.target.innerText = "O");
+      if (!e.target.innerText)
+         Game.moveNum % 2 !== 0
+            ? (e.target.innerText = "X")
+            : (e.target.innerText = "O");
 
-        Game.moveNum++;
-        board[squareIndex[0]][squareIndex[1]] = e.target.innerText;
-        Game.checkWinning();
-    };
+      Game.moveNum++;
+      board[squareIndex[0]][squareIndex[1]] = e.target.innerText;
+      Game.checkWinning();
+   };
 
-    _squares.forEach((square) =>
-        square.addEventListener("click", __updateSquare, false)
-    );
+   _squares.forEach((square) =>
+      square.addEventListener("click", __updateSquare, false)
+   );
 
-    return {
-        gameContainer,
-        board,
-        firstRow,
-        secondRow,
-        thirdRow,
-    };
+   return {
+      gameContainer,
+      board,
+      firstRow,
+      secondRow,
+      thirdRow,
+   };
 })();
