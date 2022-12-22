1698a4799f50c3d5d378203a0511a4c3ccc34c4b
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-22 18:17:19 +0100

    Get the name of the players when clicking start btn

--- scripts/form.js
+++ scripts/form.js
@@ -1,18 +1,30 @@
 const form = (() => {
-   let form = document.querySelector(".information"),
-      startBtn = document.getElementById("start-btn");
+   let formContainer = document.querySelector(".information"),
+      startBtn = document.getElementById("start-btn"),
+      firstPlayer = document.getElementById("first-player"),
+      secondPlayer = document.getElementById("second-player");
 
    const __showGameboard = (e) => {
-      e.preventDefault();
-      form.id = "information-fade-out";
+      if (firstPlayer.value && secondPlayer.value) {
+         e.preventDefault();
+         formContainer.id = "information-fade-out";
 
-      setTimeout(() => {
-         form.style.display = "none";
-         GameBoard.gameContainer.id = "gameboard-fade-in";
-         Game.playersInfo.id = "players-info-fade-in";
-         document.body.style.justifyContent = "start";
-      }, 1000);
+         setTimeout(() => {
+            formContainer.style.display = "none";
+            GameBoard.gameContainer.id = "gameboard-fade-in";
+            Game.playersInfo.id = "players-info-fade-in";
+            document.body.style.justifyContent = "start";
+         }, 1000);
+
+         form.firstPlayer = firstPlayer.value;
+         form.secondPlayer = secondPlayer.value;
+      }
    };
 
    startBtn.onclick = __showGameboard;
+
+   return {
+      firstPlayer: firstPlayer.value,
+      secondPlayer: secondPlayer.value,
+   };
 })();
