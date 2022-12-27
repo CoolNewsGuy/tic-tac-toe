dba2d049a7d6de0b4a80cd343be766fdabc95305
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-27 21:44:53 +0100

    Make a copy of the board in minimax function to avoid editing the original board

--- scripts/game.js
+++ scripts/game.js
@@ -156,6 +156,13 @@ const Game = (() => {
    // ! Minimax
    const minimax = (currentBoardState, currentPlayingMark) => {
       let availableSpots = GameBoard.getArrayOfEmptySpots();
+
+      // *Create a copy of the board to avoid editing the original board
+      let boardCopy = [
+         GameBoard.firstRow.slice(0),
+         GameBoard.secondRow.slice(0),
+         GameBoard.thirdRow.slice(0),
+      ];
    };
 
    return {
