d5fc002ce0759580731cf2ae3af3d77c63db618c
MrLuckyCat <joewilliamdd31@gmail.com>
2023-01-04 10:20:59 +0100

    Add a function to get the current state of the board and save it in virtualBoard variable

--- scripts/minmax.js
+++ scripts/minmax.js
@@ -1,7 +1,33 @@
 const Minimax = (() => {
+   // ! This will be used for recursive testing
+   let virtualBoard;
+
    return {
+      getCurrentBoardState,
       getCurrentEmptySpots,
+      virtualBoard,
+      isFirstTimeTesting: true,
    };
+
+   function getCurrentBoardState() {
+      switch (Minimax.isFirstTimeTesting) {
+         // * Create a copy of the board just for the first test
+         case true:
+            let boardCopy = [
+               GameBoard.firstRow.slice(0),
+               GameBoard.secondRow.slice(0),
+               GameBoard.thirdRow.slice(0),
+            ];
+
+            Minimax.isFirstTimeTesting = false;
+            Minimax.virtualBoard = boardCopy;
+            break;
+
+         // * For the second test and so on, we'll be getting the current state of the virtualBoard
+         case false:
+            return Minimax.virtualBoard;
+      }
+   }
 
    function getCurrentEmptySpots(currentBoardState) {
       let arr = [];
@@ -17,15 +43,6 @@ const Minimax = (() => {
 
    // ! Minimax where AI is MAXIMIZER and Human is MINIMIZER
    const minimax = (currentBoardState, currentPlayingMark) => {
-      let availableSpots = GameBoard.getArrayOfEmptySpots();
-
-      // * Create a copy of the board to avoid editing the original board
-      let boardCopy = [
-         GameBoard.firstRow.slice(0),
-         GameBoard.secondRow.slice(0),
-         GameBoard.thirdRow.slice(0),
-      ];
-
       let moveNum = Game.moveNum;
 
       // + Check if AI won
